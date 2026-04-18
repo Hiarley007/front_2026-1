cb3b38e494319f3d7ff34a20000d1e0a92cc0c77
Hiarley007 <hiarleymorais82@gmail.com>
2026-04-18 16:13:58 -0300

    Cooreção de erros no Menu

--- praticas/aluno-online-react/src/components/Menu.jsx
+++ praticas/aluno-online-react/src/components/Menu.jsx
@@ -4,27 +4,27 @@ function Menu({ navegaPara }) {
       <nav className="max-h-screen bg-gray-300 border-r border-gray-200 p-8">
         <ul>
           <li className="px-3 py-2 rounded-lg hover:bg-gray-100 cursor-pointer font-medium">
-            <a href="#" onClick={(e) => navegaPara(1)}>Dashboard</a>
+            <a href="#" onClick={() => navegaPara(1)}>Dashboard</a>
           </li>
 
           <li className="px-3 py-2 rounded-lg hover:bg-gray-100 cursor-pointer">
-            <a href="#" onClick={(e) => navegaPara(2)}>Notas</a>
+            <a href="#" onClick={() => navegaPara(2)}>Notas</a>
           </li>
 
           <li className="px-3 py-2 rounded-lg hover:bg-gray-100 cursor-pointer">
-            <a href="#" onClick={(e) => navegaPara(3)}>Faltas</a>
+            <a href="#" onClick={() => navegaPara(3)}>Faltas</a>
           </li>
 
           <li className="px-3 py-2 rounded-lg hover:bg-gray-100 cursor-pointer">
-            <a href="#" onClick={(e) => navegaPara(4)}>Boletos</a>
+            <a href="#" onClick={() => navegaPara(4)}>Boletos</a>
           </li>
 
           <li className="px-3 py-2 rounded-lg hover:bg-gray-100 cursor-pointer">
-            <a href="#" onClick={(e) => navegaPara(5)}>Requerimentos</a>
+            <a href="#" onClick={() => navegaPara(5)}>Requerimentos</a>
           </li>
 
           <li className="px-3 py-2 rounded-lg hover:bg-red-100 text-red-600 cursor-pointer mt-6">
-            <a href="#" onClick={(e) => navegaPara(0)}>Sair</a>
+            <a href="#" onClick={() => navegaPara(0)}>Sair</a>
           </li>
         </ul>
       </nav>
