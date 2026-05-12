8da6d1da22a944abf81006dec266668aac6ad4fa
Hiarley007 <hiarleymorais82@gmail.com>
2026-05-12 10:42:34 -0300

    Adicionando contexto de Autenticação

--- aulas/aula04/src/App.jsx
+++ aulas/aula04/src/App.jsx
@@ -6,19 +6,24 @@ import Home from "./pages/Home";
 import Erro404 from "./pages/Erro404";
 import Layout from "./layout/Layout";
 import Login from "./pages/Login";
+import { useAuth } from "./contexts/AuthContext";
 
 function App() {
+  const {logado} = useAuth();
+
   return (
     <Routes>
-      <Route path ="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="perfil/:id" element={<Perfil />} />
-        <Route path="settings" element={<Settings />} />
-        <Route path="*" element={<Erro404 />} />
-      </Route>
-
-      <Route path="/login" element={<Login />} />
+      {logado ? (
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="perfil/:id" element={<Perfil />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+      ) : (
+        <Route path="/login" element={<Login />} />
+      )}
+      <Route path="*" element={<Erro404 />} />
     </Routes>
   );
 }
--- aulas/aula04/src/pages/Login.jsx
+++ aulas/aula04/src/pages/Login.jsx
@@ -1,12 +1,21 @@
 import { useNavigate } from "react-router";
+import { useAuth } from "../contexts/AuthContext";
 
 function Login () {
+    const {login} = useAuth();
     const navigate = useNavigate ();
+
+    const handleEntrar = () => {
+        login({username:"hiarley@iesb.edu", password:'123456'});
+        navigate("/")
+    }
+
 
     return (
     <>
     <h1>Login</h1>
-    <button onClick={() => navigate("/")}>Entrar</button>
+    
+    <button onClick={handleEntrar}>Entrar</button>
     </>
 
 )}
