150f78757033f27e70e6b92c409ca99742bafc18
Hiarley007 <hiarleymorais82@gmail.com>
2026-05-18 08:49:58 -0300

    Resolvendo conflitos

--- praticas/aluno-online-react/src/App.jsx
+++ praticas/aluno-online-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+
 import Dashboard from "./pages/Dashboard";
 import Faltas from "./pages/Faltas";
 import Notas from "./pages/Notas";
@@ -7,16 +7,20 @@ import Requerimentos from "./pages/Requerimentos";
 import Login from "./pages/Login";
 
 function App () {
-  const [pagina, setPagina] = useState (0);
-
-  switch(pagina){
-    case 1: return <Dashboard navegaPara={setPagina}/>
-    case 2: return <Notas navegaPara={setPagina}/>
-    case 3: return <Faltas navegaPara={setPagina}/>
-    case 4: return <Boletos navegaPara={setPagina}/>
-    case 5: return <Requerimentos navegaPara={setPagina}/>
-    default: return <Login navegaPara={setPagina}/>
-  }
+  return (
+    <Routes>
+     <Route path="/" element={<Layout />}>
+        <Route index element={<Dashboard />}/>
+        <Route path="notas" element={<Notas />}/>
+        <Route path="faltas" element={<Faltas />}/>
+        <Route path="boletos" element={<Boletos />}/>
+        <Route path="requerimentos" element={<Requerimentos />}/>
+     </Route>
+        
+    <Route path="*" element={<Erro404 />}/>
+    <Route path="/login" element={<Login />} />
+    </Routes>
+  ) 
 }
 
 export default App;
