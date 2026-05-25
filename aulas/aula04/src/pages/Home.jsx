dfbcb347de6203837600a22d0848a539c519b70c
Hiarley007 <hiarleymorais82@gmail.com>
2026-05-25 10:36:32 -0300

    Adicionando listagem com Loading State

--- aulas/aula04/src/pages/Home.jsx
+++ aulas/aula04/src/pages/Home.jsx
@@ -1,10 +1,84 @@
-
+import { useEffect, useState } from "react";
 
 function Home() {
+  const [carregando, setCarregando] = useState(true);
+  const [tarefas, setTarefas] = useState([]);
+
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setTarefas([
+        { id: 1, nome: "Implementar Trabalho", situacao: "Aberto" },
+        { id: 2, nome: "Entregar Práticas", situacao: "Pendente" },
+        { id: 3, nome: "Revisar Conteúdos", situacao: "Aberto" },
+        { id: 4, nome: "Assistir Vídeos-Aulas", situacao: "Concluída" },
+      ]);
+
+      setCarregando(false);
+
+    }, 3000);
+
+    return () => clearTimeout(timeout)
+  }, [],
+  );
   return (
     <>
       <h1>Página Principal</h1>
-      
+      {carregando ? (
+        <table className="border rounded border-gray-300 animateq-pulse">
+        <thead className="bg-gray-300">
+          <tr>
+            <th><div className="p-2 h-4"></div></th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+              <td><div className="p-1 m-1 h-2.5 rounded-full bg-gray-300 w-4"></div></td>
+              <td><div className="p-1 m-1 h-2.5 rounded-full bg-gray-300 w-32"></div></td>
+              <td><div className="p-1 m-1 h-2.5 rounded-full bg-gray-300 w-16"></div></td>
+            </tr>
+
+            <tr>
+              <td><div className="p-1 m-1 h-2.5 rounded-full bg-gray-300 w-4"></div></td>
+              <td><div className="p-1 m-1 h-2.5 rounded-full bg-gray-300 w-32"></div></td>
+              <td><div className="p-1 m-1 h-2.5 rounded-full bg-gray-300 w-16"></div></td>
+            </tr>
+
+            <tr>
+              <td><div className="p-1 m-1 h-2.5 rounded-full bg-gray-300 w-4"></div></td>
+              <td><div className="p-1 m-1 h-2.5 rounded-full bg-gray-300 w-32"></div></td>
+              <td><div className="p-1 m-1 h-2.5 rounded-full bg-gray-300 w-16"></div></td>
+            </tr>
+
+            <tr>
+              <td><div className="p-1 m-1 h-2.5 rounded-full bg-gray-300 w-4"></div></td>
+              <td><div className="p-1 m-1 h-2.5 rounded-full bg-gray-300 w-32"></div></td>
+              <td><div className="p-1 m-1 h-2.5 rounded-full bg-gray-300 w-16"></div></td>
+            </tr>
+        </tbody>
+      </table>
+      ) : ( 
+      <table className="border rounded border-gray-300">
+        <thead className="bg-gray-300">
+          <tr>
+            <th className="p-2">ID</th>
+            <th>Tarefa</th>
+            <th>Situação</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tarefas.map((item) => (
+            <tr key={item.id}>
+              <td className="p-2 m-1">{item.id}</td>
+              <td className="p-2 m-1">{item.nome}</td>
+              <td className="p-2 m-1">{item.situacao}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      )}
     </>
   );
 }
--- aulas/aula04/src/pages/Perfil.jsx
+++ aulas/aula04/src/pages/Perfil.jsx
@@ -22,20 +22,22 @@ function Perfil() {
       minLength: { value: 3, message: "Nome deve ter mais de 2 caracteres" },
     },
 
-    email: {
-      min: { value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 
-      message: "Telefone é um número" },
+    email: { 
+      required: "E-mail é Obrigatŕio"
     },
 
     nascimento: {
-      validate: (value) => Date.parse(value) < Date.now() || "Data de Nascimento Inválida",
+      validate: (value) => Date.parse(`${value} 00:00:00 UTC`) < Date.now() || "Data de Nascimento Inválida",
       },
 
     telefone: {
-      min: { value: /^(?:\+)[0-9]{2}\s?(?:\()[0-9]{2}(?:\))\s?[0-9]{4,5}(?:-)[0-9]{4}$/, 
-      message: "Telefone é um número" },
-    },
-  };
+      min: {value: 0, message:"Telefone é um número"},
+      pattern: {
+          value: /^(?:\+)[0-9]{2}\s?(?:\()[0-9]{2}(?:\))\s?[0-9]{4,5}(?:-)[0-9]{4}$/,
+          message: "Telefone é um número"
+      }    
+  }
+}
 
   return (
     <>
@@ -59,7 +61,7 @@ function Perfil() {
             id="email"
             {...register("email", validadores.email)}
           />
-          {errors?.email && <p>{errors.email.message}</p>}
+          {errors?.email && <p>{errors.email.required}</p>}
         </div>
 
         <div>
