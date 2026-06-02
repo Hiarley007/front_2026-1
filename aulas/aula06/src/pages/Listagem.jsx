f32a77059bdce8f9158cb02ba1892a7b9ff87c0c
Hiarley007 <hiarleymorais82@gmail.com>
2026-06-02 10:57:23 -0300

    Adicionando front e backend

---
+++ aulas/aula06/src/pages/Listagem.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router";
+import { listar } from "../services/protudoService";
+
+function Listagem() {
+  const navigate = useNavigate();
+  const [dados, setDados] = useState([]);
+
+  useEffect(() => {
+    const carregar = async () => {
+      const resposta = await listar();
+      setDados(resposta);
+    };
+    carregar();
+  }, []);
+  return (
+    <>
+      <h1>Listagem de Produtos</h1>
+      <button onClick={() => navigate("/produtos/novo")}>Novo</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Nome do Produto</th>
+            <th>Preço</th>
+            <th>Unidade</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dados.map((item) => (
+            <tr key={item.id}>
+              <td>{item.nome}</td>
+              <td>{item.preco}</td>
+              <td>{item.unidade}</td>
+              <td>
+                <Link to={`/produtos/editar/${item.id}`}>Editar</Link>|
+                <Link to="/produtos">Remover</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default Listagem;
